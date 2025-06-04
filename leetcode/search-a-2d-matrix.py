0f1073b2659da24f77af1dec7d551f8d41a2595f
Joell Kebret <147110325+joellkebret@users.noreply.github.com>
2025-06-04 09:17:16 -0400

    Update solution for search-a-2d-matrix

--- leetcode/search-a-2d-matrix.py
+++ leetcode/search-a-2d-matrix.py
@@ -1,6 +1,28 @@
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # get dimensions of the matrix 
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        # let low be first row, and high be last
+        low = 0
+        high = rows*cols-1
+
+        while low <= high:
+            mid = (low+high)//2 # middle row
+            # This step allows us to view the matrix almost as a giant list
+            # row = mid//cols find the middle row
+            # col = mid % cols the column we should move to based on position
+            row,col = mid//cols, mid%cols 
+            guess = matrix[row][col]
+
+            if guess == target:
+                return True
+            
+            elif guess > target:
+                high = mid -1
+            
+            else:
+                low = mid + 1
         
-    }
-};
+        return False
